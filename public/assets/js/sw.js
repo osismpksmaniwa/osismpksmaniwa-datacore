6ccd7876e66954103cf4fa2286d2aa426e67daeb
Rengga Nugroho <rengga.prakoso@gmail.com>
2019-07-09 16:58:59 +0700

    Still confuse how to PWA_-

--- public/assets/js/sw.js
+++ public/assets/js/sw.js
@@ -1,26 +1,23 @@
-self.addEventListener('install', function(e) {
+var cacheName = 'osismpksmaniwa-board';
+var filesToCache = [
+    '/',
+    'js/',
+    'css/',
+    'index.html'
+];
+
+self.addEventListener('install', function (e) {
     e.waitUntil(
-      caches.open('video-store').then(function(cache) {
-        return cache.addAll([
-          '/',
-          'index.html',
-          'index.js',
-          'assets/',
-          'assets/js/main.js',
-          'assets/js/pwa.js',
-          'assets/js/util.js',
-          'assets/css/main.css'
-        ]);
-      })
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
     );
-   });
-   
-   self.addEventListener('fetch', function(e) {
-     console.log(e.request.url);
-     e.respondWith(
-       caches.match(e.request).then(function(response) {
-         return response || fetch(e.request);
-       })
-     );
-   });
-   
+});
+
+self.addEventListener('fetch', function (e) {
+    e.responWidth(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
